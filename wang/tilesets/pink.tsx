3dc8bdbac0fca930f3001e88eacc12d3c285b007
sendorak <58896154+sendorak@users.noreply.github.com>
2020-01-17 16:50:36 +1030

    Add files via upload

---
+++ wang/tilesets/pink.tsx
@@ -0,0 +1,204 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="pink" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="pink.png" width="128" height="128"/>
+ <wangsets>
+  <wangset name="standard" tile="-1">
+   <wangedgecolor name="" color="#00007f" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#005500" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00aa00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#aaff00" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="0x2020202"/>
+   <wangtile tileid="3" wangid="0x1050601"/>
+   <wangtile tileid="4" wangid="0x2040302"/>
+   <wangtile tileid="5" wangid="0x3010302"/>
+   <wangtile tileid="6" wangid="0x3050202"/>
+   <wangtile tileid="7" wangid="0x6040101"/>
+   <wangtile tileid="8" wangid="0x2040104"/>
+   <wangtile tileid="9" wangid="0x1010101"/>
+   <wangtile tileid="10" wangid="0x1050205"/>
+   <wangtile tileid="11" wangid="0x1010305"/>
+   <wangtile tileid="12" wangid="0x2020604"/>
+   <wangtile tileid="13" wangid="0x6020601"/>
+   <wangtile tileid="14" wangid="0x6020205"/>
+   <wangtile tileid="15" wangid="0x3010104"/>
+  </wangset>
+  <wangset name="sparse" tile="-1">
+   <wangedgecolor name="" color="#00007f" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#0000ff" tile="-1" probability="4"/>
+   <wangedgecolor name="" color="#005500" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00aa00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#aaff00" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="0x2020202"/>
+   <wangtile tileid="3" wangid="0x1050601"/>
+   <wangtile tileid="4" wangid="0x2040302"/>
+   <wangtile tileid="5" wangid="0x3010302"/>
+   <wangtile tileid="6" wangid="0x3050202"/>
+   <wangtile tileid="7" wangid="0x6040101"/>
+   <wangtile tileid="8" wangid="0x2040104"/>
+   <wangtile tileid="9" wangid="0x1010101"/>
+   <wangtile tileid="10" wangid="0x1050205"/>
+   <wangtile tileid="11" wangid="0x1010305"/>
+   <wangtile tileid="12" wangid="0x2020604"/>
+   <wangtile tileid="13" wangid="0x6020601"/>
+   <wangtile tileid="14" wangid="0x6020205"/>
+   <wangtile tileid="15" wangid="0x3010104"/>
+  </wangset>
+  <wangset name="dense" tile="-1">
+   <wangedgecolor name="" color="#00007f" tile="-1" probability="4"/>
+   <wangedgecolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#005500" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00aa00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#aaff00" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="0x2020202"/>
+   <wangtile tileid="3" wangid="0x1050601"/>
+   <wangtile tileid="4" wangid="0x2040302"/>
+   <wangtile tileid="5" wangid="0x3010302"/>
+   <wangtile tileid="6" wangid="0x3050202"/>
+   <wangtile tileid="7" wangid="0x6040101"/>
+   <wangtile tileid="8" wangid="0x2040104"/>
+   <wangtile tileid="9" wangid="0x1010101"/>
+   <wangtile tileid="10" wangid="0x1050205"/>
+   <wangtile tileid="11" wangid="0x1010305"/>
+   <wangtile tileid="12" wangid="0x2020604"/>
+   <wangtile tileid="13" wangid="0x6020601"/>
+   <wangtile tileid="14" wangid="0x6020205"/>
+   <wangtile tileid="15" wangid="0x3010104"/>
+  </wangset>
+  <wangset name="standard_tall" tile="-1">
+   <wangedgecolor name="" color="#00007f" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#005500" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00aa00" tile="-1" probability="4"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="4"/>
+   <wangedgecolor name="" color="#aaff00" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="0x2020202"/>
+   <wangtile tileid="3" wangid="0x1050601"/>
+   <wangtile tileid="4" wangid="0x2040302"/>
+   <wangtile tileid="5" wangid="0x3010302"/>
+   <wangtile tileid="6" wangid="0x3050202"/>
+   <wangtile tileid="7" wangid="0x6040101"/>
+   <wangtile tileid="8" wangid="0x2040104"/>
+   <wangtile tileid="9" wangid="0x1010101"/>
+   <wangtile tileid="10" wangid="0x1050205"/>
+   <wangtile tileid="11" wangid="0x1010305"/>
+   <wangtile tileid="12" wangid="0x2020604"/>
+   <wangtile tileid="13" wangid="0x6020601"/>
+   <wangtile tileid="14" wangid="0x6020205"/>
+   <wangtile tileid="15" wangid="0x3010104"/>
+  </wangset>
+  <wangset name="standard_wide" tile="-1">
+   <wangedgecolor name="" color="#00007f" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#005500" tile="-1" probability="4"/>
+   <wangedgecolor name="" color="#00aa00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#aaff00" tile="-1" probability="4"/>
+   <wangtile tileid="1" wangid="0x2020202"/>
+   <wangtile tileid="3" wangid="0x1050601"/>
+   <wangtile tileid="4" wangid="0x2040302"/>
+   <wangtile tileid="5" wangid="0x3010302"/>
+   <wangtile tileid="6" wangid="0x3050202"/>
+   <wangtile tileid="7" wangid="0x6040101"/>
+   <wangtile tileid="8" wangid="0x2040104"/>
+   <wangtile tileid="9" wangid="0x1010101"/>
+   <wangtile tileid="10" wangid="0x1050205"/>
+   <wangtile tileid="11" wangid="0x1010305"/>
+   <wangtile tileid="12" wangid="0x2020604"/>
+   <wangtile tileid="13" wangid="0x6020601"/>
+   <wangtile tileid="14" wangid="0x6020205"/>
+   <wangtile tileid="15" wangid="0x3010104"/>
+  </wangset>
+  <wangset name="sparse_tall" tile="-1">
+   <wangedgecolor name="" color="#00007f" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#0000ff" tile="-1" probability="4"/>
+   <wangedgecolor name="" color="#005500" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00aa00" tile="-1" probability="16"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="16"/>
+   <wangedgecolor name="" color="#aaff00" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="0x2020202"/>
+   <wangtile tileid="3" wangid="0x1050601"/>
+   <wangtile tileid="4" wangid="0x2040302"/>
+   <wangtile tileid="5" wangid="0x3010302"/>
+   <wangtile tileid="6" wangid="0x3050202"/>
+   <wangtile tileid="7" wangid="0x6040101"/>
+   <wangtile tileid="8" wangid="0x2040104"/>
+   <wangtile tileid="9" wangid="0x1010101"/>
+   <wangtile tileid="10" wangid="0x1050205"/>
+   <wangtile tileid="11" wangid="0x1010305"/>
+   <wangtile tileid="12" wangid="0x2020604"/>
+   <wangtile tileid="13" wangid="0x6020601"/>
+   <wangtile tileid="14" wangid="0x6020205"/>
+   <wangtile tileid="15" wangid="0x3010104"/>
+  </wangset>
+  <wangset name="sparse_wide" tile="-1">
+   <wangedgecolor name="" color="#00007f" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#0000ff" tile="-1" probability="4"/>
+   <wangedgecolor name="" color="#005500" tile="-1" probability="16"/>
+   <wangedgecolor name="" color="#00aa00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#aaff00" tile="-1" probability="16"/>
+   <wangtile tileid="1" wangid="0x2020202"/>
+   <wangtile tileid="3" wangid="0x1050601"/>
+   <wangtile tileid="4" wangid="0x2040302"/>
+   <wangtile tileid="5" wangid="0x3010302"/>
+   <wangtile tileid="6" wangid="0x3050202"/>
+   <wangtile tileid="7" wangid="0x6040101"/>
+   <wangtile tileid="8" wangid="0x2040104"/>
+   <wangtile tileid="9" wangid="0x1010101"/>
+   <wangtile tileid="10" wangid="0x1050205"/>
+   <wangtile tileid="11" wangid="0x1010305"/>
+   <wangtile tileid="12" wangid="0x2020604"/>
+   <wangtile tileid="13" wangid="0x6020601"/>
+   <wangtile tileid="14" wangid="0x6020205"/>
+   <wangtile tileid="15" wangid="0x3010104"/>
+  </wangset>
+  <wangset name="dense_tall" tile="-1">
+   <wangedgecolor name="" color="#00007f" tile="-1" probability="4"/>
+   <wangedgecolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#005500" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00aa00" tile="-1" probability="16"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="16"/>
+   <wangedgecolor name="" color="#aaff00" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="0x2020202"/>
+   <wangtile tileid="3" wangid="0x1050601"/>
+   <wangtile tileid="4" wangid="0x2040302"/>
+   <wangtile tileid="5" wangid="0x3010302"/>
+   <wangtile tileid="6" wangid="0x3050202"/>
+   <wangtile tileid="7" wangid="0x6040101"/>
+   <wangtile tileid="8" wangid="0x2040104"/>
+   <wangtile tileid="9" wangid="0x1010101"/>
+   <wangtile tileid="10" wangid="0x1050205"/>
+   <wangtile tileid="11" wangid="0x1010305"/>
+   <wangtile tileid="12" wangid="0x2020604"/>
+   <wangtile tileid="13" wangid="0x6020601"/>
+   <wangtile tileid="14" wangid="0x6020205"/>
+   <wangtile tileid="15" wangid="0x3010104"/>
+  </wangset>
+  <wangset name="dense_wide" tile="-1">
+   <wangedgecolor name="" color="#00007f" tile="-1" probability="4"/>
+   <wangedgecolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#005500" tile="-1" probability="16"/>
+   <wangedgecolor name="" color="#00aa00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#aaff00" tile="-1" probability="16"/>
+   <wangtile tileid="1" wangid="0x2020202"/>
+   <wangtile tileid="3" wangid="0x1050601"/>
+   <wangtile tileid="4" wangid="0x2040302"/>
+   <wangtile tileid="5" wangid="0x3010302"/>
+   <wangtile tileid="6" wangid="0x3050202"/>
+   <wangtile tileid="7" wangid="0x6040101"/>
+   <wangtile tileid="8" wangid="0x2040104"/>
+   <wangtile tileid="9" wangid="0x1010101"/>
+   <wangtile tileid="10" wangid="0x1050205"/>
+   <wangtile tileid="11" wangid="0x1010305"/>
+   <wangtile tileid="12" wangid="0x2020604"/>
+   <wangtile tileid="13" wangid="0x6020601"/>
+   <wangtile tileid="14" wangid="0x6020205"/>
+   <wangtile tileid="15" wangid="0x3010104"/>
+  </wangset>
+ </wangsets>
+</tileset>
