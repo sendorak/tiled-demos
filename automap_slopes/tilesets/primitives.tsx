fb57aa8f2d6fc6c4c26bdc92f02ed689175eb36a
sendorak <58896154+sendorak@users.noreply.github.com>
2020-01-19 13:59:38 +1030

    Add files via upload

---
+++ automap_slopes/tilesets/primitives.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="primitives" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="primitives.png" width="256" height="256"/>
+ <terraintypes>
+  <terrain name="primitives" tile="18"/>
+ </terraintypes>
+ <tile id="8" terrain="0,0,0,"/>
+ <tile id="9" terrain=",0,0,"/>
+ <tile id="10" terrain="0,0,,0"/>
+ <tile id="11" terrain=",,,0"/>
+ <tile id="12" terrain=",,0,0"/>
+ <tile id="13" terrain=",,0,"/>
+ <tile id="19" terrain=",0,,0"/>
+ <tile id="20" terrain="0,0,0,0"/>
+ <tile id="21" terrain="0,,0,"/>
+ <tile id="24" terrain="0,,0,0"/>
+ <tile id="25" terrain="0,,,0"/>
+ <tile id="26" terrain=",0,0,0"/>
+ <tile id="27" terrain=",0,,"/>
+ <tile id="28" terrain="0,0,,"/>
+ <tile id="29" terrain="0,,,"/>
+</tileset>
